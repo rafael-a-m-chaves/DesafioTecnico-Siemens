dbf8f68ad3d6d8af89f79a283fac2633370d8ef1
Rafael Chaves <rafaelaugustochaves@hotmail.com>
2022-12-01 14:15:44 -0300

    Config startup

--- DesafioTecnico.API/DesafioTecnico.API/Startup.cs
+++ DesafioTecnico.API/DesafioTecnico.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DesafioTecnico.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,16 +27,31 @@ namespace DesafioTecnico.API
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<APIContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+
+
+            services.AddSwaggerGen();
+
+            services.AddCors();
             services.AddControllers();
         }
 
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseCors(option => option
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
---
+++ DesafioTecnico.API/DesafioTecnico.Infrastructure/Context/APIContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioTecnico.Infrastructure.Context
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options) : base(options) { }
+
+
+        //public virtual DbSet<Cliente> Cliente { get; set; }
+        //public virtual DbSet<Cidade> Cidade { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
